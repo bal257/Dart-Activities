2449a23b21fd6c28820ce4e5563a8a6c18a9ff6f
carbonara1818 <crystaljoyagbayani05@gmail.com>
2025-07-29 23:43:15 +0800

    update

--- practice.dart
+++ practice.dart
@@ -3,14 +3,36 @@ import 'dart:io';
 void main()
 {
   int flag = 0;
+  String? name , pass;
+
   print('\n\x1B[34m===REGISTRATION===\x1B[0m\n');
 
-  stdout.write('\x1B[33mEnter your username: \x1B[0m');
-  String? name = stdin.readLineSync();
+ // name = getUsername(name);
 
-  stdout.write('\x1B[33mEnter your password: \x1B[0m');
-  String? pass = stdin.readLineSync();
+ do{
+  name = getUsername(name);
 
+  if (name == null || name.isEmpty) {
+    print('\x1B[31mUsername cannot be empty!\x1B[0m');
+    continue; // Prompt for username again
+  }
+
+  pass = getPassword(pass);
+
+  if (pass == null || pass.isEmpty) {
+    print('\x1B[31mPassword cannot be empty!\x1B[0m');
+    continue; // Prompt for password again
+  }
+  else if(pass.length < 8)
+  {
+    print("Password must be at least 8 characters long!");
+    continue;
+  }
+  
+ 
+ } while (name == null || name.isEmpty || pass == null || pass.isEmpty || pass.length < 8);
+  
+  
   print('\n\x1B[32mRegistration successful for user:\x1B[0m $name');
 
   print('\n\x1B[34m===LOGIN===\x1B[0m\n');
@@ -25,7 +47,7 @@ do{
   if(logname == name && logpass == pass)
   {
     print('\x1B[32mLogin Successfully!\x1B[0m');
-    flag == 0;
+    flag = 0;
     break;
   }
   else
@@ -37,3 +59,57 @@ do{
 
   print('\nHello, $name!');
 } 
+
+String? getUsername(String? name)
+{
+  name = null;
+  stdout.write('\x1B[33mEnter your username: \x1B[0m');
+  return name = stdin.readLineSync();
+}
+
+String? getPassword(String? pass)
+{
+  pass = null;
+  stdout.write('\x1B[33mEnter your password: \x1B[0m');
+  return pass = stdin.readLineSync();
+}
+
+// import 'dart:io';
+
+// void main()
+// {
+//   int flag = 0;
+//   print('\n\x1B[34m===REGISTRATION===\x1B[0m\n');
+
+//   stdout.write('\x1B[33mEnter your username: \x1B[0m');
+//   String? name = stdin.readLineSync();
+
+//   stdout.write('\x1B[33mEnter your password: \x1B[0m');
+//   String? pass = stdin.readLineSync();
+
+//   print('\n\x1B[32mRegistration successful for user:\x1B[0m $name');
+
+//   print('\n\x1B[34m===LOGIN===\x1B[0m\n');
+
+// do{
+//   stdout.write('\x1B[33mEnter your  username: \x1B[0m');
+//   String? logname = stdin.readLineSync();
+
+//   stdout.write('\x1B[33mEnter your password: \x1B[0m');
+//   String? logpass = stdin.readLineSync();
+
+//   if(logname == name && logpass == pass)
+//   {
+//     print('\x1B[32mLogin Successfully!\x1B[0m');
+//     flag == 0;
+//     break;
+//   }
+//   else
+//   {
+//     print('\n\x1B[31mLogin Failed! Please check your username and password.\x1B[0m\n');
+//     flag = 1;
+//   }
+// } while (flag == 1);
+
+//   print('\nHello, $name!');
+// } 
